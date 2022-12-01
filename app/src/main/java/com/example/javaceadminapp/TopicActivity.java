8e3fd989117d1bd6ea6893452b2c1fef1326df8d
LauPeeps <pilapil.203348@gensan.sti.edu.ph>
2022-12-02 03:50:35 +0800

    Secure this

--- app/src/main/java/com/example/javaceadminapp/TopicActivity.java
+++ app/src/main/java/com/example/javaceadminapp/TopicActivity.java
@@ -2,10 +2,13 @@ package com.example.javaceadminapp;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -19,107 +22,45 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Objects;
+
 public class TopicActivity extends AppCompatActivity {
 
     FirebaseFirestore firestore;
-    TextView topicTitle, topicContent;
-    Button questionBtn, showDialog, updateTopic;
-    EditText name, content;
-    Dialog topicUpdate;
-    String subid, subname, moduleid;
+    String subid, moduleid;
+    Dialog progressDialog;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_topic);
 
         Bundle bundle = getIntent().getExtras();
-
-
         subid = bundle.getString("subid");
         moduleid = bundle.getString("moduleid");
 
-        topicTitle = findViewById(R.id.topicTitle);
-        topicContent = findViewById(R.id.topicContent);
-
         firestore = FirebaseFirestore.getInstance();
 
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Topics");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        topicUpdate = new Dialog(TopicActivity.this);
-        topicUpdate.setContentView(R.layout.edit_topic_dialog);
-        topicUpdate.setCancelable(true);
-        topicUpdate.getWindow().setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
-
-        name = topicUpdate.findViewById(R.id.editTopicTitle);
-        content = topicUpdate.findViewById(R.id.editTopicContent);
-        updateTopic = topicUpdate.findViewById(R.id.updateTopicBtn);
-
-        questionBtn = findViewById(R.id.qBtn);
-        showDialog = findViewById(R.id.showEditDialog);
-
-        fetchTopic();
-
-        questionBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(TopicActivity.this, Questions.class);
-                intent.putExtra("module", moduleid);
-                intent.putExtra("submodule", subid);
-                startActivity(intent);
-                finish();
-
-            }
-        });
-
-        showDialog.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                name.setText(subid);
-                content.setText(subname);
-                topicUpdate.show();
-            }
-        });
-
-        updateTopic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                updateTopic(name.getText().toString(), content.getText().toString());
-                fetchTopic();
-            }
-        });
-
-
-
-        topicContent.setMovementMethod(new ScrollingMovementMethod());
+        progressDialog = new Dialog(TopicActivity.this);
+        progressDialog.setContentView(R.layout.loading_progressbar);
+        progressDialog.setCancelable(false);
+        progressDialog.getWindow().setBackgroundDrawableResource(R.drawable.progressbar_background);
+        progressDialog.getWindow().setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
 
     }
 
-    private void fetchTopic() {
-        firestore.collection("Quizzes").document(moduleid)
-                .collection(subid).document("Topic_List").get()
-                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-                    @Override
-                    public void onSuccess(DocumentSnapshot documentSnapshot) {
-                        topicTitle.setText(documentSnapshot.getString("topic_title"));
-                        topicContent.setText(documentSnapshot.getString("topic_content"));
-                    }
-                });
-    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
 
-    private void updateTopic(String topicT, String topicC) {
-        topicUpdate.dismiss();
-        firestore.collection("Quizzes").document(moduleid).collection(subid).document("Topic_List")
-                .update("topic_title", topicT, "topic_content", topicC).addOnSuccessListener(new OnSuccessListener<Void>() {
-                    @Override
-                    public void onSuccess(Void unused) {
-                        Toast.makeText(TopicActivity.this, "Updated Successfully", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(TopicActivity.this, "Update Failed", Toast.LENGTH_SHORT).show();
-                    }
-                });
+        return super.onOptionsItemSelected(item);
     }
-
 }
